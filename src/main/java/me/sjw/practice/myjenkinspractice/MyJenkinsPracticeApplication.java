09399ce2e011a1a7d10554f4684a821057e86fa1
Xavier Jongwook Seong <ypwa121@gmail.com>
2025-07-05 18:59:12 +0900

    init commit

---
+++ src/main/java/me/sjw/practice/myjenkinspractice/MyJenkinsPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.sjw.practice.myjenkinspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJenkinsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJenkinsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sjw/practice/myjenkinspractice/MyJenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sjw.practice.myjenkinspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJenkinsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
